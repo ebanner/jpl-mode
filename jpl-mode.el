648dbb54d9fbcfd40ce7510c178a0c2a762a15df
Edward Banner <edward.banner@gmail.com>
2026-07-30 19:05:36 -0400

    Make apl-> a function

--- jpl-mode.el
+++ jpl-mode.el
@@ -17,28 +17,49 @@
         ("⍺" . "x")
         ))
 
-(setq apl-str "{≢ ⍺ ↑ {:⍤1 ⍋⍨ (≢,⊃) ⌸⍨ ⍵}")
-
-(setq apl-str
- (let ((regexp (regexp-opt (mapcar #'car pairs))))
-   (replace-regexp-in-string
-    regexp
-    (lambda (match)
-      (pcase (assoc match pairs)
-        (`(,_ . ,new) new)))
-    apl-str)))
-
 (setq pairs2
       '(("*tail*" . "{:")
         ))
 
-(setq apl-str
- (let ((regexp (regexp-opt (mapcar #'car pairs2))))
-   (replace-regexp-in-string
-    regexp
-    (lambda (match)
-      (pcase (assoc match pairs2)
-        (`(,_ . ,new) new)))
-    apl-str)))
+(defun apl->j (apl-str)
 
-apl-str
+  (setq apl-str
+        (let ((regexp (regexp-opt (mapcar #'car pairs))))
+          (replace-regexp-in-string
+           regexp
+           (lambda (match)
+             (pcase (assoc match pairs)
+               (`(,_ . ,new) new)))
+           apl-str)))
+
+  (let ((regexp (regexp-opt (mapcar #'car pairs2))))
+    (replace-regexp-in-string
+     regexp
+     (lambda (match)
+       (pcase (assoc match pairs2)
+         (`(,_ . ,new) new)))
+     apl-str)))
+
+(apl->j "{≢ ⍺ ↑ {:⍤1 ⍋⍨ (≢,⊃) ⌸⍨ ⍵}")
+
+(defun my-send-line-to-j ()
+  (interactive)
+
+  (let* ((text (string-trim
+                (if (use-region-p)
+                    (buffer-substring (region-beginning) (region-end))
+                  (thing-at-point 'line t))))
+         (apl-text (apl->j text))
+         (win (get-buffer-window "*J*")))
+
+    (message apl-text)
+
+    (with-current-buffer (get-buffer "*J*")
+      (goto-char (point-max))
+      (insert apl-text)
+      (comint-send-input))
+
+    (when win
+      (set-window-point win (with-current-buffer "*J*" (point-max))))))
+
+(global-set-key (kbd "TAB") #'my-send-line-to-j)
